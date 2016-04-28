193f8c023222403c910a0213562bcaa0e2d790e0
Alex Boyd <m.alexander.boyd@gmail.com>
2016-04-28 13:02:16 -0400

    Fuck git

--- src/client.java
+++ src/client.java
@@ -1,52 +1,49 @@
 import java.awt.Color;
+import java.sql.Date;
+import java.sql.ResultSet;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JList;
 
 public class client extends JFrame{
-	private JTable table;
-	private JTable table_1;
+	private JTable girthTable;
 	private JTable table_2;
-	public client() {
+	private JTable skinTable;
+	private JTable weekTable;
+	private int clientID;
+	public client(int client) {
+	
+	clientID = client;
+		setupTables();
 	
 	// might need a re-work to better lay it out 
 	setTitle("MetabolixLite: Client Manager");
 	getContentPane().setBackground(new Color(255, 235, 205));
 	getContentPane().setLayout(null);
 	
-	JScrollPane fold = new JScrollPane();
-	fold.setBounds(10, 71, 414, 71);
-	getContentPane().add(fold);
-	
-	table = new JTable();
-	fold.setViewportView(table);
-	
-	JScrollPane girth = new JScrollPane();
-	girth.setBounds(10, 177, 414, 71);
-	getContentPane().add(girth);
-	
-	table_1 = new JTable();
-	girth.setViewportView(table_1);
-	
 	JScrollPane mealPlan = new JScrollPane();
-	mealPlan.setBounds(10, 281, 414, 71);
+	mealPlan.setBounds(12, 377, 414, 71);
 	getContentPane().add(mealPlan);
 	
 	table_2 = new JTable();
 	mealPlan.setViewportView(table_2);
 	
-	JLabel lblNewLabel = new JLabel("Skin Fold Assessments:");
-	lblNewLabel.setBounds(10, 46, 111, 14);
-	getContentPane().add(lblNewLabel);
+	JLabel weekDataLabel = new JLabel("Progress Data:");
+	weekDataLabel.setBounds(12, 114, 116, 14);
+	getContentPane().add(weekDataLabel);
 	
-	JLabel lblGirthMeasurements = new JLabel("Girth Measurement:");
-	lblGirthMeasurements.setBounds(10, 152, 111, 14);
-	getContentPane().add(lblGirthMeasurements);
+	JLabel lblAssessmentTst = new JLabel("Assessment Tests:");
+	lblAssessmentTst.setBounds(12, 223, 160, 14);
+	getContentPane().add(lblAssessmentTst);
 	
 	JLabel lblMealPlan = new JLabel("Meal Plan:");
-	lblMealPlan.setBounds(10, 256, 111, 14);
+	lblMealPlan.setBounds(22, 351, 111, 14);
 	getContentPane().add(lblMealPlan);
 	
 	JButton btnNewButton = new JButton("New Week");
@@ -290,4 +287,8 @@ public class client extends JFrame{
 	
 		
 	}
+	private DefaultTableModel weekTableModel;
+	private DefaultTableModel skinTableModel;
+	private DefaultTableModel girthTableModel;
+	private int ClientID;
 }

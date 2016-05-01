4ff7ebb86599d03b01d716e0bb1e9b2efdcc87ab
Tzvi Zeller <tzvi3115@aol.com>
2016-05-01 18:34:06 -0400

    ddfs

--- src/sendForm.java
+++ src/sendForm.java
@@ -1,19 +1,27 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.sql.ResultSet;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class sendForm extends JFrame{
-	public sendForm() {
+	
+	private int clientID;
+		
+	public sendForm(int client) {
+		clientID = client;
 		getContentPane().setBackground(new Color(255, 235, 205));
 		getContentPane().setLayout(null);
+		setMinimumSize(new Dimension(365, 315));
+		setMaximumSize(new Dimension(365, 315));
 		
 		JButton submit = new JButton("Send");
+		
 		submit.setBounds(123, 249, 89, 23);
 		getContentPane().add(submit);
 		
@@ -21,32 +29,64 @@ public class sendForm extends JFrame{
 		label.setBounds(29, 168, 330, 20);
 		getContentPane().add(label);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Health Questionnaire");
-		chckbxNewCheckBox.setBackground(new Color(255, 235, 205));
-		chckbxNewCheckBox.setBounds(29, 215, 156, 23);
-		getContentPane().add(chckbxNewCheckBox);
+		final JCheckBox checkBox_health = new JCheckBox("Health Questionnaire");
+		checkBox_health.setBackground(new Color(255, 235, 205));
+		checkBox_health.setBounds(29, 215, 156, 23);
+		getContentPane().add(checkBox_health);
 		
-		JCheckBox chckbxEnvironmentalInventory = new JCheckBox("Environmental Inventory");
-		chckbxEnvironmentalInventory.setBackground(new Color(255, 235, 205));
-		chckbxEnvironmentalInventory.setBounds(187, 189, 156, 23);
-		getContentPane().add(chckbxEnvironmentalInventory);
+		final JCheckBox checkBox_envi = new JCheckBox("Environmental Inventory");
+		checkBox_envi.setBackground(new Color(255, 235, 205));
+		checkBox_envi.setBounds(187, 189, 156, 23);
+		getContentPane().add(checkBox_envi);
 		
-		JCheckBox checkBox_2 = new JCheckBox("Liability Waiver");
-		checkBox_2.setBackground(new Color(255, 235, 205));
-		checkBox_2.setBounds(29, 189, 156, 23);
-		getContentPane().add(checkBox_2);
+		final JCheckBox checkBox_Lia = new JCheckBox("Liability Waiver");
+		checkBox_Lia.setBackground(new Color(255, 235, 205));
+		checkBox_Lia.setBounds(29, 189, 156, 23);
+		getContentPane().add(checkBox_Lia);
 		
-		JCheckBox chckbxClientReimbursement = new JCheckBox("Client Reimbursement");
-		chckbxClientReimbursement.setBackground(new Color(255, 235, 205));
-		chckbxClientReimbursement.setBounds(187, 215, 156, 23);
-		getContentPane().add(chckbxClientReimbursement);
+		final JCheckBox checkbox_Reimbursement = new JCheckBox("Client Reimbursement");
+		checkbox_Reimbursement.setBackground(new Color(255, 235, 205));
+		checkbox_Reimbursement.setBounds(187, 215, 156, 23);
+		getContentPane().add(checkbox_Reimbursement);
 		
 		JLabel icon = new JLabel("");
 		icon.setIcon(new ImageIcon(sendForm.class.getResource("/images/logo.png")));
 		icon.setBounds(73, 0, 198, 169);
 		getContentPane().add(icon);
 		setTitle("MetabolixLite: Form Sender");
+		
+		submit.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				attemptFormSend(clientID,checkBox_health.isSelected(),checkBox_envi.isSelected(),checkBox_Lia.isSelected(), checkbox_Reimbursement.isSelected());
+				setVisible(false);
+			}
+		});
+		
+		pack();
+		
+	}//ends send form
+		
+	//Method to validate input and send to DB
+	private static void attemptFormSend(int client, boolean health, boolean envi, boolean lia, boolean Reimbursement) {
+		try {
+		
+		//need to add methods to force insert into all fields....will add this last 
+	
+		//code to send to DB
+		sqlMethods sql = new sqlMethods();
+		ResultSet email;
+		email = sql.getClientEmail(client);
+		while(email.next()){
+			String clientEmail=email.getString(1);
+			
+			//call email function here (will set file needed in mail)
 
-	}
+			}
+		
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	
+	} // Ends attemptFormSend
 
-}
+}//end

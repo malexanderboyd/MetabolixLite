8ca02185c2a77d2a469f8a4abec5424b83548dd7
M. Alex Boyd <Alex@malexanderboyd.com>
2016-05-02 23:43:16 -0400

    Added Client tables generation

---
+++ client.java
@@ -0,0 +1,410 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.sql.Date;
+import java.sql.ResultSet;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JList;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class client extends JFrame{
+	private JTable girthTable;
+	private JTable fitnessPlanTable;
+	private JTable skinTable;
+	private JTable weekTable;
+	private int clientID;
+	public client(int client) {
+	
+	clientID = client;
+		setupTables();
+		updateTables(clientID);
+		generateFitnessPlans();
+		
+	System.out.println(clientID);
+	// might need a re-work to better lay it out 
+	setTitle("MetabolixLite: Client Manager");
+	getContentPane().setBackground(new Color(255, 235, 205));
+	getContentPane().setLayout(null);
+	
+	JScrollPane mealPlan = new JScrollPane();
+	mealPlan.setBounds(12, 377, 605, 71);
+	getContentPane().add(mealPlan);
+	
+	fitnessPlanTable = new JTable();
+	fitnessPlanTable.setModel(fitnessPlanTableModel);
+	mealPlan.setViewportView(fitnessPlanTable);
+	
+	JLabel weekDataLabel = new JLabel("Progress Data:");
+	weekDataLabel.setBounds(12, 114, 116, 14);
+	getContentPane().add(weekDataLabel);
+	
+	JLabel lblAssessmentTst = new JLabel("Assessment Tests:");
+	lblAssessmentTst.setBounds(12, 223, 160, 14);
+	getContentPane().add(lblAssessmentTst);
+	
+	JLabel lblMealPlan = new JLabel("Fitness Plans:");
+	lblMealPlan.setBounds(12, 351, 111, 14);
+	getContentPane().add(lblMealPlan);
+	
+	JButton btnNewButton = new JButton("New Week");
+	btnNewButton.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			addWeek aw = new addWeek(clientID);
+			aw.setBounds(400,0,450,600);
+			aw.setVisible(true);
+		}
+	});
+	btnNewButton.setBounds(82, 34, 117, 23);
+	getContentPane().add(btnNewButton);
+
+	JTabbedPane assessmentTestTabPane = new JTabbedPane(JTabbedPane.TOP);
+	assessmentTestTabPane.setBounds(22, 249, 595, 90);
+	getContentPane().add(assessmentTestTabPane);
+	
+	JScrollPane fold = new JScrollPane();
+	assessmentTestTabPane.addTab("Skin Fold", null, fold, null);
+	
+	skinTable = new JTable();
+	skinTable.setModel(skinTableModel);
+	fold.setViewportView(skinTable);
+	
+	JScrollPane girth = new JScrollPane();
+	assessmentTestTabPane.addTab("Girth", null, girth, null);
+	
+	girthTable = new JTable();
+	girthTable.setModel(girthTableModel);
+	girth.setViewportView(girthTable);
+	
+	JScrollPane scrollPane = new JScrollPane();
+	scrollPane.setBounds(12, 140, 605, 71);
+	getContentPane().add(scrollPane);
+	
+	weekTable = new JTable();
+	weekTable.setModel(weekTableModel);
+	scrollPane.setViewportView(weekTable);
+	
+	JButton sendFormBtn = new JButton("Send Forms");
+	sendFormBtn.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			sendForm sf = new sendForm(clientID);
+			sf.setVisible(true);
+			
+		}
+	});
+	sendFormBtn.setBounds(230, 33, 117, 25);
+	getContentPane().add(sendFormBtn);
+	
+	
+	setPreferredSize(new Dimension(650,650));
+	setLocationRelativeTo(null);
+
+
+	pack();
+
+	}
+	
+	
+	private void generateFitnessPlans()
+	{
+		int weeks = weekTableModel.getRowCount();
+		System.out.println(weeks + " weeks");
+		for(int i = 0; i < weeks; i++)
+		{
+			fitness_weight = (int) weekTableModel.getValueAt(i, 4);
+			fitness_height = (int) weekTableModel.getValueAt(i, 3);
+			int age = (int) weekTableModel.getValueAt(i, 2);
+			double sf = ((double) skinTableModel.getValueAt(i, 8)) + ((double) skinTableModel.getValueAt(i, 5)) + (double) skinTableModel.getValueAt(i, 7) ;
+			double density = (1.10938 - (0.0008267*sf) + 0.0000016*(sf*sf) - (0.0002574*age));
+			bf = ((4.95/density) - 4.5)*100;
+			double fw = fitness_weight * (bf/100);
+			lbw = fitness_weight - fw; // Lean body weight
+			rmr = (66+(13.75*(fitness_weight*0.45359237))+(5*(fitness_height*2.54))-(6.8*age));
+			exmr = rmr*1.375;
+			protein = ((((rmr+exmr)/2)/4)*0.3);
+			carbs = ((rmr*0.4)/9);
+			fats = ((rmr*0.3)/9);
+			int calories = (int) (rmr+200);
+			/*"Date", "Weight", "LBW", "FW", "RMR", "EX MR",
+			"Protein (g)", "Carbs (g)", "Fats (g)"*/
+			fitnessPlanTableModel.addRow(new Object[] { weekTableModel.getValueAt(i, 0), fitness_weight, lbw, fw, rmr, exmr, protein, carbs, fats, calories}); 
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private void updateTables(int ClientID)
+	{
+		weekTableModel.setRowCount(0);
+		skinTableModel.setRowCount(0);
+		girthTableModel.setRowCount(0);
+		fitnessPlanTableModel.setRowCount(0);
+
+		Thread runner = new Thread() 
+		{
+			public void run()
+			{
+				try {
+					sqlMethods sql = new sqlMethods();
+					ResultSet queryResult1;
+					
+					queryResult1 = sql.getClientWeekData(ClientID);
+					while(queryResult1.next())
+					{
+
+							String name = queryResult1.getString(2);
+							int age = queryResult1.getInt(3);
+							Date date = queryResult1.getDate(4);		
+							int ht = queryResult1.getInt(5);
+							int wt = queryResult1.getInt(6);
+							
+							weekTableModel.addRow(new Object[] { date, name, age, ht, wt });
+							System.out.println("added week");
+					}
+					//Client(Client_ID, Client_Fname, Client_Lname, Client_Age, Date_Entered (date), Height (int), Weight(int), Trainer_Uname)
+
+					queryResult1.close();
+				} catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+
+
+
+
+
+		};
+
+		
+		Thread runner2 = new Thread() 
+		{
+			public void run()
+			{
+				try {
+					sqlMethods sql = new sqlMethods();
+					ResultSet queryResult2;
+					
+					queryResult2 = sql.getClientSkinData(ClientID);
+					
+					while(queryResult2.next())
+					{
+						//Skinfold(S_Date, Arm, Waist, Calf, Hips, S_Thigh, Neck, S_Chest, S_Abdom, C_IDs)
+							Date date = queryResult2.getDate(1);		
+							arm = queryResult2.getFloat(2);
+							waist = queryResult2.getFloat(3);
+							calf = queryResult2.getFloat(4);
+							hips = queryResult2.getFloat(5);
+							skin_thigh = queryResult2.getFloat(6);
+							neck = queryResult2.getFloat(7);
+							skin_chest = queryResult2.getFloat(8);
+							skin_abdom = queryResult2.getFloat(9);
+							skinTableModel.addRow(new Object[] { date, arm, waist, calf, hips, skin_thigh, neck, skin_chest, skin_abdom });
+					}
+					
+					queryResult2.close();
+				//Skinfold(S_Date, Arm, Waist, Calf, Hips, S_Thigh, Neck, S_Chest, S_Abdom, C_IDs)
+				//"Date", "Arm", "Waist", "Calf", "Hips", "Thigh", "Neck", "Chest", "Abdom"
+				} catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+
+
+
+
+
+		};
+		
+		
+		Thread runner3 = new Thread() 
+		{
+			public void run()
+			{
+				try {
+					sqlMethods sql = new sqlMethods();
+					ResultSet queryResult3;
+					
+					queryResult3 = sql.getClientGirthData(ClientID);
+					
+					while(queryResult3.next())
+					{
+
+						Date date = queryResult3.getDate(1);		
+						midax = queryResult3.getFloat(2);
+					    subscap = queryResult3.getFloat(3);
+						tricep = queryResult3.getFloat(4);
+						kidney = queryResult3.getFloat(5);
+						supra = queryResult3.getFloat(6);
+						girth_chest = queryResult3.getFloat(7);
+						girth_thigh = queryResult3.getFloat(8);
+						girth_abdom = queryResult3.getFloat(9);
+						girthTableModel.addRow(new Object[] { date, midax, subscap, tricep, kidney, supra, girth_chest, girth_thigh, girth_abdom });
+						System.out.println("added girth");
+					}
+					//Girth(G_date, MidAx, Subscap, Triceps, Kidney, Supra, G_Chest, G_Thigh, G_Abdom, C_IDg)
+					//"Date", "Mid Ax", "Subscap", "Triceps", "Kidney", "Supra", "Chest", "Thigh", "Abdom"
+					queryResult3.close();
+				} catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		};
+
+		
+		runner.run();
+		runner2.run();
+		runner3.run();
+
+
+
+
+
+	}
+	private void setupTables()
+	{
+		//Client(Client_ID, Client_Fname, Client_Lname, Client_Age, Date_Entered, Height, Weight, Trainer_Uname)
+		fitnessPlanTableModel = new DefaultTableModel() {
+			Class[] columnTypes = new Class[] {
+					Object.class, Object.class, Object.class, Object.class, Object.class,
+					Object.class, Object.class, Object.class, Object.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			public boolean isCellEditable(int row, int column) {
+			       //all cells false
+			       return false;
+			    }
+		};
+		fitnessPlanTableModel.setColumnCount(10);
+		fitnessPlanTableModel.setColumnIdentifiers(new String[] {
+				"Date", "Weight", "LBW", "FW", "RMR", "EX MR",
+				"Protein (g)", "Carbs (g)", "Fats (g)", "Calories"
+		});
+		/*
+		 * 
+		 *  weekly data table
+		 */
+		weekTableModel = new DefaultTableModel() {
+			Class[] columnTypes = new Class[] {
+					Object.class, Object.class, Object.class, Object.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		};
+		weekTableModel.setColumnCount(5);
+		weekTableModel.setColumnIdentifiers(new String[] {
+				"Date", "Name", "Age", "Height", "Weight"
+		});
+		
+		/*
+		 * 
+		 *  girth assessment data table
+		 *  Girth(G_date, MidAx, Subscap, Triceps, Kidney, Supra, G_Chest, G_Thigh, G_Abdom, C_IDg)
+	 *	
+
+		 */	
+		
+		girthTableModel = new DefaultTableModel() {
+			Class[] columnTypes = new Class[] {
+					Object.class, Object.class, Object.class, Object.class, Object.class,
+					Object.class, Object.class, Object.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		};
+		girthTableModel.setColumnCount(9);
+		girthTableModel.setColumnIdentifiers(new String[] {
+				"Date", "Mid Ax", "Subscap", "Triceps", "Kidney", "Supra", "Chest", "Thigh", "Abdom"
+		});
+		
+		/*
+		 * 
+		 *  skin fold assessment data table
+		 *  
+	 *	Skinfold(S_Date, Arm, Waist, Calf, Hips, S_Thigh, Neck, S_Chest, S_Abdom, C_IDs)
+
+		 */	
+	
+		skinTableModel = new DefaultTableModel() {
+			Class[] columnTypes = new Class[] {
+					Object.class, Object.class, Object.class, Object.class, Object.class,
+					Object.class, Object.class, Object.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		};
+		skinTableModel.setColumnCount(5);
+		skinTableModel.setColumnIdentifiers(new String[] {
+				"Date", "Arm", "Waist", "Calf", "Hips", "Thigh", "Neck", "Chest", "Abdom"
+		});
+	
+		
+	}
+	private DefaultTableModel weekTableModel;
+	private DefaultTableModel skinTableModel;
+	private DefaultTableModel girthTableModel;
+	private DefaultTableModel fitnessPlanTableModel;
+	private int ClientID;
+	private double midax;
+	private double subscap;
+	private double tricep;
+	private double kidney;
+	private double supra;
+	private double girth_chest;
+	private double girth_thigh;
+	private double girth_abdom;
+	private double arm;
+	private double waist;
+	private double calf;
+	private double hips;
+	private double neck;
+	private double skin_thigh;
+	private double skin_chest;
+	private double skin_abdom;
+	
+	private int fitness_weight;
+	private int fitness_height;
+	private double bf;
+	private double lbw;
+	private double fw;
+	private double rmr;
+	private double exmr;
+	private double protein;
+	private double carbs;
+	private double fats;
+	
+	
+}
+
---
+++ sqlMethods.java
@@ -0,0 +1,452 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+public class sqlMethods {
+	private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+/*	private static String hostName = "triton.towson.edu";
+	private static int port = 3360;
+	private static String database = "mboyd8db";
+	private static String userName = "mboyd8";
+	private static String password = "Cosc*4h4p";
+	private static final String SALT = "tacos4lyfe"; */
+	
+	private static String hostName = "triton.towson.edu";
+	private static int port = 3360;
+	private static String database = "msanto7db";
+	private static String userName = "msanto7";
+	private static String password = "Cosc*8adm";
+	private static final String SALT = "tacos4lyfe";
+	
+	/*
+	 *  Database Tables 
+	 * 	Client(Client_ID, Client_Fname, Client_Lname, Client_Age, Date_Entered, Height, Weight, Trainer_id)
+	 *	Client_Assessment_Form (C_ID, Liability_Waiver, Environmental_Inventory, Health_History_Questionnaire)
+	 *	Girth(G_date, MidAx, Subscap, Triceps, Kidney, Supra, G_Chest, G_Thigh, G_Abdom, C_IDg)
+	 *	Skinfold(S_Date, Arm, Waist, Calf, Hips, S_Thigh, Neck, S_Chest, S_Abdom, C_IDs)
+	 *	Trainer(trainerID, T_Uname, password, email)
+	 * 
+	 * 
+	 * 
+	 */
+
+	private static String connect_URL = "jdbc:mysql://" + hostName + ":" + port + "/" + database;
+
+	public int attemptLogin(String username, String password)
+	{
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String signupQuery = "SELECT * FROM Trainer WHERE username = ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+		preparedStmt.setString(1, username);
+		
+		ResultSet rs = preparedStmt.executeQuery();
+		String retrievedpw = "n/a";
+		int clientId = -1;
+		while(rs.next())
+		{
+			retrievedpw = rs.getString("password");
+			clientId = rs.getInt("trainerID");
+		}
+		String saltedpw = SALT + password;
+		String hashedpw = generateHash(saltedpw);
+		
+
+		if(retrievedpw.equals(hashedpw))
+		{
+			preparedStmt.close();
+			conn.close();
+			return clientId;
+		}
+		else
+		{
+			preparedStmt.close();
+			conn.close();
+			return -1;
+		}
+		
+
+			
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return -1;
+		}
+
+	}
+	
+	//send client info to DB
+	public Boolean attemptNewClient2(String name, Integer age, String date, Integer height, Integer weight, Float midax, Float subscap, Float triceps, Float kidney, Float supra, Float gchest, Float gthigh, Float gabdom, Integer trainerId, Float arm, Float waist, Float calf, Float hips, Float s_thigh, Float neck, Float s_chest, Float s_abdom) {
+		
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		Statement st = conn.createStatement();
+		st.executeUpdate("insert into Client (Client_name, Client_Age, Date_Entered, Height, Weight, Trainer_id) VALUES('" + name + "', '"+ age +"', '"+ date + "', '"+ height +"', '"+ weight +"', '"+ trainerId +"')");
+		
+		//add sql statement to grab the client id to pass into the girth and skinfold table
+		String idQ = ("SELECT Client_ID FROM Client WHERE Client_name = ? AND Client_Age = ?");
+		PreparedStatement preparedStmt = conn.prepareStatement(idQ);
+		preparedStmt.setString(1, name);
+		preparedStmt.setInt(2,  age);
+		
+		ResultSet r = preparedStmt.executeQuery();
+		int cid = 0;
+		while (r.next()) {
+		cid = r.getInt("Client_ID");
+		}
+		
+		//System.out.printf("Client ID is equal to %d", cid);
+		
+		//girth
+		st.executeUpdate("insert into Girth (G_date, MidAx, Subscap, Triceps, Kidney, Supra, G_Chest, G_Thigh, G_Abdom, C_IDg) VALUES('"+ date + "', '"+ midax +"', '"+ subscap+"', '"+ triceps +"', '"+ kidney +"', '"+ supra +"', '"+ gchest +"', '"+ gthigh +"', '"+ gabdom +"', '"+ cid +"')");                                                   
+		//skinfold
+		st.executeUpdate("insert into Skinfold (S_Date, Arm, Waist, Calf, Hips, S_Thigh, Neck, S_Chest, S_Abdom, C_IDs) VALUES('"+ date +"', '"+ arm +"', '"+ waist +"', '"+ calf +"', '"+ hips +"', '"+ s_thigh + "', '"+ neck +"', '"+ s_chest +"', '"+ s_abdom +"', '"+ cid + "')");
+		
+		//ResultSet rs = preparedStmt.executeUpdate();
+		//String retrievedpw = "n/a";
+		
+		conn.close();
+		return true;
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+	public boolean attemptAddWeek(int clientID, String date, Integer weight, Float midax, Float subscap, Float triceps, Float kidney, Float supra, Float gchest, Float gthigh, Float gabdom, Float arm, Float waist, Float calf, Float hips, Float s_thigh, Float neck, Float s_chest, Float s_abdom) {
+		
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+
+		Statement st = conn.createStatement();
+		String update = ("UPDATE Client SET Date_Entered = ?, Weight = ? WHERE Client_ID = ? ");
+		PreparedStatement p = conn.prepareStatement(update);
+		p.setString(1, date);
+		p.setInt(2, weight);
+		p.setInt(3, clientID);
+		p.executeUpdate();
+		
+		//System.out.printf("Client ID is equal to %d", cid);
+		
+		//girth
+		st.executeUpdate("insert into Girth (G_date, MidAx, Subscap, Triceps, Kidney, Supra, G_Chest, G_Thigh, G_Abdom, C_IDg) VALUES('"+ date + "', '"+ midax +"', '"+ subscap+"', '"+ triceps +"', '"+ kidney +"', '"+ supra +"', '"+ gchest +"', '"+ gthigh +"', '"+ gabdom +"', '"+ clientID +"')");                                                   
+		//skinfold
+		st.executeUpdate("insert into Skinfold (S_Date, Arm, Waist, Calf, Hips, S_Thigh, Neck, S_Chest, S_Abdom, C_IDs) VALUES('"+ date +"', '"+ arm +"', '"+ waist +"', '"+ calf +"', '"+ hips +"', '"+ s_thigh + "', '"+ neck +"', '"+ s_chest +"', '"+ s_abdom +"', '"+ clientID + "')");
+		
+		conn.close();
+		return true;
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return false;
+		}
+	
+	}
+	
+	
+	public boolean attemptSignUp(String username, String password, String email)
+	{
+		java.sql.Connection conn = null;
+		java.sql.Statement stmt = null;
+		if(!checkUserExists(username))
+		{
+			try {
+				conn = DriverManager.getConnection(connect_URL, userName, this.password);
+
+				String signupQuery = "insert into Trainer (username, password, email) values (?, ?, ?)";
+
+				PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+				preparedStmt.setString(1, username);
+				preparedStmt.setString(3, email);
+
+				// hashing pw to store in db // 
+				String saltedpw = SALT + password;
+				String hashedpw = generateHash(saltedpw);
+				preparedStmt.setString(2, hashedpw); // store hashed pw in db
+				
+				preparedStmt.executeUpdate();
+				preparedStmt.close();
+				conn.close();
+				return true;
+			} catch(SQLException sqlE) {
+				sqlE.printStackTrace();
+			}
+		}
+		return false;
+	}
+
+	
+private boolean checkUserExists(String username) {
+		
+	java.sql.Connection conn = null;
+	try {
+	conn = DriverManager.getConnection(connect_URL, userName, this.password);
+	
+	String signupQuery = "SELECT * FROM Trainer WHERE username = ?";
+	
+	PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+	preparedStmt.setString(1, username);
+	
+	ResultSet rs = preparedStmt.executeQuery();
+	String retrievedpw = "n/a";
+	System.out.println(rs.toString());
+	if(!rs.next())
+	{
+		preparedStmt.close();
+		conn.close();
+		return false;
+	}
+	else
+	{
+		preparedStmt.close();
+		conn.close();
+		return true;
+	}
+
+	} catch(SQLException sqlE) {
+		sqlE.printStackTrace();
+	}
+	
+	return false;
+}
+
+
+	// used to hash pw in db
+	public static String generateHash(String input) {
+		StringBuilder hash = new StringBuilder();
+
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			byte[] hashedBytes = sha.digest(input.getBytes());
+			char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+					'a', 'b', 'c', 'd', 'e', 'f' };
+			for (int idx = 0; idx < hashedBytes.length; ++idx) {
+				byte b = hashedBytes[idx];
+				hash.append(digits[(b & 0xf0) >> 4]);
+				hash.append(digits[b & 0x0f]);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// handle error here.
+		}
+
+		return hash.toString();
+	}
+
+
+	public ResultSet getClientWeekData(int clientID) {
+
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String signupQuery = "SELECT * FROM Client WHERE Client_ID = ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+		preparedStmt.setInt(1, clientID);
+		
+		System.out.println(preparedStmt.toString());
+		ResultSet rs = preparedStmt.executeQuery();
+		return rs;
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return null;
+		}
+	}
+
+
+	public ResultSet getClientSkinData(int clientID) {
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String signupQuery = "SELECT * FROM Skinfold WHERE C_IDs = ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+		preparedStmt.setInt(1, clientID);
+		
+		ResultSet rs = preparedStmt.executeQuery();
+
+
+			return rs;
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return null;
+		}
+	}
+
+
+	public ResultSet getClientGirthData(int clientID) {
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String signupQuery = "SELECT * FROM Girth WHERE C_IDg = ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+		preparedStmt.setInt(1, clientID);
+		
+		ResultSet rs = preparedStmt.executeQuery();
+
+
+			return rs;
+
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return null;
+		}
+	}
+
+	//Client(Client_ID, Client_Fname, Client_Lname, Client_Age, Date_Entered, Height, Weight, Trainer_Uname)
+	public ResultSet getTrainerClients(int trainer)
+	{
+			java.sql.Connection conn = null;
+			try {
+			conn = DriverManager.getConnection(connect_URL, userName, this.password);
+			
+			String signupQuery = "SELECT * FROM Client WHERE Trainer_id = ?";
+			
+			PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+			preparedStmt.setInt(1, trainer);
+			
+			ResultSet rs = preparedStmt.executeQuery();
+			
+			
+			return rs;
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	//gets client email
+	public ResultSet getClientEmail(int clientID) {
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String signupQuery = "SELECT Client_email FROM Client WHERE Client_ID = ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+		preparedStmt.setInt(1, clientID);
+		
+		ResultSet rs = preparedStmt.executeQuery();
+
+			
+			return rs;
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+			return null;
+		}
+	}
+	
+	//sets form boolean in table
+	public void formSet(int clientID, boolean health, boolean envi, boolean lia) {
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String form = "UPDATE Client_Assessment_Form SET Health_History_Questionnaire= ? AND Environmental_Inventory = ? AND Liability_Waiver = ? WHERE C_ID = ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(form);
+		preparedStmt.setBoolean(1, health);
+		preparedStmt.setBoolean(2, envi);
+		preparedStmt.setBoolean(3, lia);
+		preparedStmt.setInt(4, clientID);
+	
+		preparedStmt.executeUpdate();
+		conn.close();
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+		}
+	}//end of form set
+	
+	
+	//reset password
+	public void resetPass(String email, String pass) {
+		java.sql.Connection conn = null;
+		try {
+		conn = DriverManager.getConnection(connect_URL, userName, this.password);
+		
+		String resetPass = "UPDATE Trainer SET password = ? WHERE email= ?";
+		
+		PreparedStatement preparedStmt = conn.prepareStatement(resetPass);
+		String saltedpw = SALT + pass;
+		String hashedpw = generateHash(saltedpw);
+		preparedStmt.setString(1, hashedpw);
+		preparedStmt.setString(2, email);
+	
+		preparedStmt.executeUpdate();
+
+			conn.close();
+
+		} catch(SQLException sqlE) {
+			sqlE.printStackTrace();
+		}
+	}//end of reset
+	
+	
+	
+	
+    public ResultSet getTrainerEmail(String user) {
+        java.sql.Connection conn = null;
+        try {
+        conn = DriverManager.getConnection(connect_URL, userName, this.password);
+       
+        String signupQuery = "SELECT email FROM Trainer WHERE username = ?";
+       
+        PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+        preparedStmt.setString(1, user);
+       
+        ResultSet rs = preparedStmt.executeQuery();
+ 
+    
+            return rs;
+ 
+        } catch(SQLException sqlE) {
+            sqlE.printStackTrace();
+            return null;
+        }
+    }
+	
+	public String getTrainerUsername(int trainerId)
+	{
+		 java.sql.Connection conn = null;
+	        try {
+	        conn = DriverManager.getConnection(connect_URL, userName, this.password);
+	       
+	        String signupQuery = "SELECT username FROM Trainer WHERE trainerID = ?";
+	       
+	        PreparedStatement preparedStmt = conn.prepareStatement(signupQuery);
+	        preparedStmt.setInt(1, trainerId);
+	       
+	        ResultSet rs = preparedStmt.executeQuery();
+	        rs.next();
+	    
+	        return rs.getString(1);
+	 
+	        } catch(SQLException sqlE) {
+	            sqlE.printStackTrace();
+	            return null;
+	        }
+	}
+	
+	
+	
+
+} // end of class
